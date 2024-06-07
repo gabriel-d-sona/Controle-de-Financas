a4796200717c0b3acca93f5b4ae581a6c7fe8a6b
gabriel-d-sona <gdsona@hotmail.com>
2024-06-06 21:04:27 -0300

    Header adicionado

--- src/pages/Financas.js
+++ src/pages/Financas.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "../componentes/Header";
 import Controle from "../componentes/Controle";
 
 class Financas extends React.Component {
     render() {
         return (
-            <Controle />
+            <div className="container">
+                <Header />
+                <Controle />
+            </div>
         )
     }
 }
